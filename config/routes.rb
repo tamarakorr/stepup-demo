506b02d16af939ec36d91c4de4f6d8c4dddaf0bd
Tammy Orr <tamarakorr@gmail.com>
2014-08-25 12:30:21 +0100

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 StepupDemo::Application.routes.draw do
+  get "users/new"
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
   root  'static_pages#home'
+  match '/register',to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "User pages" do
+  subject { page }
+
+  describe "register page" do
+    before { visit register_path }
+
+    it { should have_content('Register') }
+    it { should have_title('StepUp Demo | Register') }
+  end
+end
