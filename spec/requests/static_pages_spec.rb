6cd8481741ea9cc9a1f6a19c750960f6ef60efd3
Tammy Orr <tamarakorr@gmail.com>
2014-08-25 11:38:28 +0100

    Add layout partials

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,4 +45,18 @@ describe "Static pages" do
       expect(page).to have_title("StepUp Demo | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("StepUp Demo | Contact")
+    end
+  end
+
 end
