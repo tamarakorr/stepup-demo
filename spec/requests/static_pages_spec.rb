b95d49a44a5746bcb28bae13b0442cb7a5c04795
Tammy Orr <tamarakorr@gmail.com>
2014-08-25 12:02:42 +0100

    Fill in static links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,62 +1,39 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'StepUp Demo'" do
-      visit root_path
-      expect(page).to have_content('StepUp Demo')
-    end
+    it { should have_content('StepUp Demo') }
+    it { should have_title('StepUp Demo') }
+    it { should_not have_title('Home') }
 
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("StepUp Demo")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("StepUp Demo | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title('StepUp Demo | Help') }
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("StepUp Demo | About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title('StepUp Demo | About Us') }
   end
 
   describe "Contact page" do
 
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("StepUp Demo | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title('StepUp Demo | Contact') }
   end
 
 end
