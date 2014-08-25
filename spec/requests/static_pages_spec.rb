c86a2f0c93e28aa6ffc1a01545d1aca80143df14
Tammy Orr <tamarakorr@gmail.com>
2014-08-25 09:49:55 +0100

    Fixed header & bootstrap static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,36 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('StepUp Demo')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("StepUp Demo | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("StepUp Demo | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("StepUp Demo | About Us")
+    end
   end
 end
