3b92921a1bcf1efed77f82a9bfe182c2c2ede83d
Tammy Orr <tamarakorr@gmail.com>
2014-08-29 20:17:36 +0100

    Continue debugging failing precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ StepupDemo::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true #false tko 29 Aug 2014 trying to debug precompile
 
   # Generate digests for assets URLs.
   config.assets.digest = true
