0d82d2517c7af633b44b1214d24e98a696cbee73
Tammy Orr <tamarakorr@gmail.com>
2014-08-30 11:11:53 +0100

    Add volunteer sorting

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,9 +1,9 @@
 class VolunteersController < ApplicationController
   before_action :signed_in_user  #, only: [:create, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
-    @volunteers = Volunteer.order(cause: :asc, work_region: :asc,
-      service: :asc).paginate(page: params[:page], per_page: 15)
+    @volunteers = Volunteer.order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 15)
   end
 
   def new
@@ -33,5 +33,13 @@ class VolunteersController < ApplicationController
     def volunteer_params
       params.require(:volunteer).permit(:user_id,:cause,:work_region,:skill,:availability,
         :service,:remote_status,:contact_mobile,:contact_city,:contact_country)
+    end
+    def sort_column
+      # Sort by service if nothing specified
+      Volunteer.column_names.include?(params[:sort]) ? params[:sort] : "service"
+    end
+    
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
