653ddd6ac824ea7a8888986a6722fe07bfd13e79
Tammy Orr <tamarakorr@gmail.com>
2014-08-30 10:53:05 +0100

    Add vacancy sorting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Also see http://railscasts.com/episodes/228-sortable-table-columns
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-down" : "icon-chevron-up"}'></i>".html_safe,
+      {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
