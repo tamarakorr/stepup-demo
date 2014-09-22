267fe782b7bc159c65210181ad3990bb7b431dd2
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2014-09-22 17:57:02 +0530

    vacancies can now to sorted based on NGO's name

--- spec/controllers/vacancies_controller_spec.rb
+++ spec/controllers/vacancies_controller_spec.rb
@@ -10,10 +10,30 @@ describe VacanciesController do
     }
 
     let!(:vacancies){[
-      FactoryGirl.create(:vacancy, :work_region => 'India', :service => 'Serivce 1'),
-      FactoryGirl.create(:vacancy, :work_region => 'US', :service => 'Serivce 2'),
-      FactoryGirl.create(:vacancy, :work_region => 'Germany', :service => 'Serivce 3'),
-      FactoryGirl.create(:vacancy, :work_region => 'US', :service => 'Serivce 4')
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'India',
+        :service => 'Serivce 1',
+        :ngo => FactoryGirl.create(:ngo, :name => 'ngo 4')
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'US',
+        :service => 'Serivce 2',
+        :ngo => FactoryGirl.create(:ngo, :name => 'ngo 1')
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'Germany',
+        :service => 'Serivce 3',
+        :ngo => FactoryGirl.create(:ngo, :name => 'ngo 3')
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'US',
+        :service => 'Serivce 4',
+        :ngo => FactoryGirl.create(:ngo, :name => 'ngo 2')
+      )
     ]}
 
     context "when there are no sort parameters" do
@@ -34,6 +54,18 @@ describe VacanciesController do
         response.should be_success
         assigns(:vacancies).pluck(:work_region)
           .should == vacancies.map(&:work_region).sort.reverse
+      end
+
+      context "sorting by ngo's name" do
+        it "should sort based on NGO's name" do
+          get 'index',
+              :sort => 'ngos.name',
+              :direction => 'asc'
+          response.should be_success
+          assigns(:vacancies).pluck('ngos.name').should == vacancies.map{ |vacancy|
+            vacancy.ngo.name
+          }.sort
+        end
       end
     end
   end
