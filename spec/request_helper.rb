f8124977b21b08300b88d9ec192351878e5db0f8
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2014-09-22 17:15:55 +0530

    request helper file added to for assitance methods in specs

---
+++ spec/request_helper.rb
@@ -0,0 +1,21 @@
+def clean!(tables=false)
+  # Ideal use for this method is for the Spec classes to clean DB 
+  #   and email deliveries or anything else that has to be deleted.
+
+  # This method will clean the tables that you pass in the argument array.
+  #   if nothing is passed, it will clean all the tables that are defined below.
+  #     Also, this method clears are email deliveries.
+
+  tables ||=  %w{
+                  vacancies
+                }
+
+  tables.each do |table|
+    ActiveRecord::Base.connection.execute("TRUNCATE #{table};")
+  end
+  reset_email_deliveries
+end
+
+def reset_email_deliveries
+  ActionMailer::Base.deliveries = []
+end
