e0d27ee5733141590a407bf2e6047f58b2ffeab6
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2014-09-23 11:18:20 +0530

    Filter methods on vacancies model added for filtering.

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -38,8 +38,27 @@ class Vacancy < ActiveRecord::Base
     return false if field_name.nil?
     return true if column_names.include?(field_name)
     # In case of sorting on NGO's fields, the field will contain 'ngo.' appended to it.    
-    #   e.g: 'ngo.name'. So remove that and send to NGO model.
+    #   e.g: 'ngos.name'. So remove that and send to NGO model.
     return true if Ngo.sortable_by?(field_name.split('.').last) 
+  end
+
+  def self.filter(filters = {})
+    records = Vacancy.eager_load(:ngo)
+    filters.stringify_keys!
+    
+    if filters['work_region'].present?
+      records = records.where("vacancies.work_region in (?)", filters['work_region'])
+    end
+
+    if filters['service'].present?
+      records = records.where("vacancies.service in (?)", filters['service'])
+    end
+
+    if filters['ngos.name'].present?
+      records = records.where("ngos.name in (?)", filters['ngos.name'])
+    end
+
+    records
   end
 
 end
--- spec/models/vacancy_spec.rb
+++ spec/models/vacancy_spec.rb
@@ -1,5 +1,139 @@
 require 'spec_helper'
+require 'request_helper'
 
 describe Vacancy do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".filters" do
+
+    before(:all){
+      clean!
+    }
+
+    let!(:ngos){[
+      FactoryGirl.create(:ngo, :name => 'ngo 1'),
+      FactoryGirl.create(:ngo, :name => 'ngo 2'),
+      FactoryGirl.create(:ngo, :name => 'ngo 3')
+    ]}
+
+    let!(:vacancies){[
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'India',
+        :service => 'Service 1',
+        :ngo => ngos[0]
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'UK',
+        :service => 'Service 2',
+        :ngo => ngos[1]
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'India',
+        :service => 'Service 3',
+        :ngo => ngos[0]
+      ),
+      FactoryGirl.create(
+        :vacancy,
+        :work_region => 'US',
+        :service => 'Service 1',
+        :ngo => ngos[2]
+      )
+    ]}
+
+    context "when no filters are passed" do
+      it "should return all records" do
+        results = Vacancy.filter
+        results.count.should be_eql(vacancies.size)
+      end
+    end
+
+    context "work_region filter" do
+      context "when the filter is empty" do
+        it "should return all records" do
+          results = Vacancy.filter({ :work_region => [] })
+          results.count.should be_eql(vacancies.size)
+        end
+      end
+
+      context 'when the filter is NOT empty' do
+        it "should return matching records" do
+          results = Vacancy.filter({
+            :work_region => [
+              "India",
+              "UK"
+            ]
+          })
+          results.pluck(:id).should =~ vacancies[0..2].map(&:id)
+        end
+      end
+    end
+
+    context "service filter" do
+      context "when the filter is empty" do
+        it "should return all records" do
+          results = Vacancy.filter({ :service => [] })
+          results.count.should be_eql(vacancies.size)
+        end
+      end
+
+      context 'when the filter is NOT empty' do
+        it "should return matching records" do
+          results = Vacancy.filter({
+            :service => [
+              "Service 1",
+              "Service 2"
+            ]
+          })
+          results.pluck(:id).should =~ vacancies.select{ |vacancy|
+            ["Service 1", "Service 2"].include?(vacancy.service)
+          }.map(&:id)
+        end
+      end
+    end
+
+    context "ngos.name filter" do
+      context "when the filter is empty" do
+        it "should return all records" do
+          results = Vacancy.filter({ "ngos.name" => [] })
+          results.count.should be_eql(vacancies.size)
+        end
+      end
+
+      context 'when the filter is NOT empty' do
+        it "should return matching records" do
+          results = Vacancy.filter({
+            "ngos.name" => [
+              "ngo 2",
+              "ngo 3"
+            ]
+          })
+          results.pluck(:id).should =~ vacancies.select{ |vacancy|
+            ["ngo 2", "ngo 3"].include?(vacancy.ngo.name)
+          }.map(&:id)
+        end
+      end
+    end
+
+    context "when multiple filters are passed togethter" do
+      it "should return the matching records" do
+        results = Vacancy.filter({
+          :service => [
+            "Service 1",
+            "Service 2"
+          ],
+          :work_region => [
+            "India",
+            "UK"
+          ],
+          :'ngos.name' => [
+            "ngo 2",
+            "ngo 3"
+          ]
+        })
+
+        results.pluck(:id).should =~ [vacancies[1].id]
+      end
+    end
+  end
 end
