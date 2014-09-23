a28fd60253d8fa2ea546bd7b24f82f774afc743f
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2014-09-23 12:12:40 +0530

    sorting can be done over existing filters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,11 +11,19 @@ module ApplicationHelper
   end
 
   # Also see http://railscasts.com/episodes/228-sortable-table-columns
-  def sortable(column, title = nil)
+
+  
+  def sortable(column, title = nil, to_be_persited_params = {})
+    # to_be_persited_params is added to save existing parameters 
+    #   in the url like in case of filters. e.g., user has filtered
+    #     and then sorted, we want to sort over the filtered values.
+    # 
+    to_be_persited_params ||= {}
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    params_to_pass = to_be_persited_params.merge({:sort => column, :direction => direction})
     link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-down" : "icon-chevron-up"}'></i>".html_safe,
-      {:sort => column, :direction => direction}, {:class => css_class}
+      params_to_pass, {:class => css_class}
   end
 end
